aadf2b5258267f6215cea5f62fe7841191b7443a
Danila Rozhevskii <68590625+drozhevskii@users.noreply.github.com>
2023-07-11 03:56:49 -0400

    Update twitch_bot.py

--- twitch_bot.py
+++ twitch_bot.py
@@ -1,13 +1,13 @@
 from twitchio.ext import commands
 import os
 
-from lilly_bot import lilly, read_chat
+from chatGPT_bot import chatGPT, read_chat
 
-oauth_token = 'TWITCH APO KEY'
+oauth_token = 'TWITCH API KEY'
 client_id = 'TWITCH CLIENT ID'
 
 
-bot_account = 'Lilly'
+bot_account = 'YOUR BOT NAME' # in case you have multiple bots connected to the same chat
 channel_name = 'CHANNEL NAME'
 
 class Bot(commands.Bot):
@@ -23,26 +23,22 @@ class Bot(commands.Bot):
     
     async def event_ready(self):
         # Notify us when everything is ready!
-        # We are logged in and ready to chat and use commands...
+        # We are logged in and ready to chat and use commands
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
 
     async def event_message(self, message):
-        # Messages with echo set to True are messages sent by the bot...
-        # For now we just want to ignore them...
+        # Messages with echo set to True are messages sent by the bot
+        # For now we just want to ignore them
         if message.echo:
             return
 
-        # Print the contents of our message to console...
+        # Print the contents of our message to console
         #self.incoming_msg = message.content
         print('Chat: ', message.content)
         read_chat(message.content)
         print(lilly(message.content))
-        #print(message.content)
-
-        # Since we have commands and are overriding the default `event_message`
-        # We must let the bot know we want to handle and invoke our commands...
-        #await self.handle_commands(message)
+        
 
     @commands.command()
     async def hello(self, ctx: commands.Context):
